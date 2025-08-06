6363dffff09a4bb0252ef42668c913c81351ca4a
vishalmansuriya <vishalmansuriya2004@gmail.com>
2025-08-06 16:26:57 +0530

    java

---
+++ age1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class age1 {
+    public static void main(String[] args) {
+        // System.out.println("My age is " + age);
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        System.out.println("My age is " + age);
+    }
+}
