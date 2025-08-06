96a6962e522da5efc81cc080a9c0e3cfebfbbb30
vishalmansuriya <vishalmansuriya2004@gmail.com>
2025-08-06 16:28:38 +0530

    fix

--- age1.java
+++ age1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class age1 {
+public class Age1 {
     public static void main(String[] args) {
         // System.out.println("My age is " + age);
         Scanner sc = new Scanner(System.in);
